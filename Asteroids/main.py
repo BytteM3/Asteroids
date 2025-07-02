51a9608ad4fe2f63b2619ca330d022ff76412a35
BytteM3 <mateo666@live.com>
2025-07-02 05:46:14 +0200

    movement

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,9 +1,14 @@
 import pygame
 from constants import *
+from player import *
 
 pygame.init()
+clock = pygame.time.Clock()
+dt = 0
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 running = True
+player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
 
 # Game loop
 while running:
@@ -11,8 +16,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+
+
     screen.fill("black")
+    player.update(dt)
+    player.draw(screen)
     pygame.display.flip()
+    dt = clock.tick(60) / 1000
 
 def main():
     print("Starting Asteroids!")
